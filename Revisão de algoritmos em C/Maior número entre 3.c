ffef179d49326360949ae98ec4cf783e6f55f1d6
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2024-05-28 11:53:04 -0300

    Update Maior número entre 3.c

--- Revisão de algoritmos em C/Maior número entre 3.c
+++ Revisão de algoritmos em C/Maior número entre 3.c
@@ -1,18 +1,22 @@
 /******************************************************************************
-(3) Escreva um programa que receba tres nuumeros e mostre o maior;
+(3) Escreva um programa que receba três números e mostre o maior;
 *******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-    int i,c;
-    printf ("Escreva dois numeros: \n");
-    scanf ("%d %d", &i, &c);
+    int a,b,c;
+    printf ("Escreva três numeros: \n");
+    scanf ("%d %d %d", &a, &b, &c);
    
-   if (i > c) {
-       printf("O maior numero eh: %d\n", i);
+   if (a > b && a > c) {
+       printf("O maior numero eh: %d\n", a);
    }
-       else if (c > i) {
+       else if (b > a && b > c) {
+       printf("O maior numero eh: %d\n", c);
+   }
+    
+    else if (c > a && c > b) {
        printf("O maior numero eh: %d\n", c);
    }
   else {
@@ -21,4 +25,3 @@ int main()
    
     return 0;
 }
-
