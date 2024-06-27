758dfd04c0e8560ec5ac0f86a4cd96500d9949ef
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2024-06-27 11:25:25 -0300

    Create Elementos no vetor.c

---
+++ Repetição, Vetores, Matrizes e Strings/Elementos no vetor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    int N,x,i,v[100];
+while (x >= 0) {
+  v[N] = x;
+  N++;
+scanf ("%d", &x);
+}
+
+printf ("O vetor possui %d elementos\n", N).
+  for (i= N-1; i >= 0; i++) {
+    printf ("%d\n", v[i-1])
+  }
+
+return 0;
+}
